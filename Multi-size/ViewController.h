bebdd1568176dafa66c1cff1951f04a87fc2a195
danoqualls <mynameisdano@gmail.com>
2014-12-16 13:11:37 -0500

    Initial commit

---
+++ Multi-size/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Multi-size
+//
+//  Created by Dano on 12/15/14.
+//  Copyright (c) 2014 Dano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
